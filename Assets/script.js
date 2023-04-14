5447f479b3926cfc6cb6eddecf9715cd7852df4d
Max <max.belenchuk@gmail.com>
2023-04-14 11:32:47 -0300

    defined all variables from HTML to script.js. Created a function that grabs api of current weather and forcasted weather using api key, and then fetching forecast. Next step is to loop trough the data with the data provided from the api

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,51 @@
-function weatherApi() {
-    var weatherApiUrl = "https://api.openweathermap.org/data/3.0/onecall?&appid=e3aa746e7c535bde01009eef4a333aa2"
-     fetch(weatherApiUrl)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (dataWeather) {
-            console.log(dataWeather)
-            
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      
+var searchBtn = document.querySelectorById('searchBtn');
+var clearBtn = document.querySelectorById('remove-search');
+var tempElement = document.querySelectorById('temperature');
+var humidityElement = document.querySelectorById('humidity');
+var cityElement = document.querySelectorById('city');
+var uvElement = document.querySelectorById('UV'); 
+var windElement = document.querySelectorById('wind');
+var fiveDays = document.querySelectorById('fiveDaysForecast');
+var apiKey = 'e3aa746e7c535bde01009eef4a333aa2'
+
+
+function getAPI (city) {
+  currentApi = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + '&appid=' + apiKey;
+  forecastApi = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial" + '&appid=' + apiKey;
+
+
+fetch(forecastApi)
+.then(function(response){
+  if (response.ok) {
+    return response.json();
   }
+  throw response.json()
+}) 
+
   
-  weatherApi();
+
+}
+
+
+
+
+
+
+
+// function weatherApi() {
+//     var weatherApiUrl = "https://api.openweathermap.org/data/3.0/onecall?&appid=e3aa746e7c535bde01009eef4a333aa2"
+//      fetch(weatherApiUrl)
+//       .then(function (response) {
+//         if (response.ok) {
+//           response.json().then(function (dataWeather) {
+//             console.log(dataWeather)
+            
+//           });
+//         } else {
+//           alert('Error: ' + response.statusText);
+//         }
+//       })
+      
+//   }
+  
+//   weatherApi();
